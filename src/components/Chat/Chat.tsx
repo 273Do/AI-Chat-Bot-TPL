afcb9b767b9fd1afd2bf41867de2ba8261100a26
273* <keita.y310@icloud.com>
2024-12-31 01:31:02 +0900

    💄  Inputエリアのスタイルを修正しました．

---
+++ src/components/Chat/Chat.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="m-2 h-[1200px] bg-slate-700">
+      <div>test</div>
+    </div>
+  );
+};
+
+export default Chat;
--- src/components/Chat/ChatScreen.tsx
+++ src/components/Chat/ChatScreen.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Chat from "./Chat";
 import * as CS from "./index";
 
 // チャット画面全体のコンポーネント
@@ -11,10 +12,11 @@ const ChatScreen = () => {
         <div className="flex w-[800px] max-w-6xl flex-col items-center">
           <div className="h-16"></div>
           <div className="flex w-full flex-col justify-center">
-            <div className="h-[1200px] w-full bg-slate-400 text-xl">Chat</div>
+            <div className="size-full bg-slate-400">
+              <Chat />
+            </div>
           </div>
-          <div className="h-4"></div>
-          <div className="h-28 w-full rounded-t-lg bg-muted"></div>
+          <div className="h-32"></div>
         </div>
         <CS.Footer />
       </div>
